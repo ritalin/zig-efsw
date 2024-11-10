e6ef71533860e638ea628edc96cb5b88490365e8
Ritalin <kazu.ktz.tamura@gmail.com>
2024-11-10 21:15:06 +0900

    minor fix

--- build.zig
+++ build.zig
@@ -21,7 +21,6 @@ pub fn build(b: *std.Build) !void {
         .name = "efsw_core",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
